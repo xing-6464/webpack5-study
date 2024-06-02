c20ad409bd31e662e10a6a403a2f0f518d0f3e9d
xing-6464 <17621404964@163.com>
2024-06-02 23:54:58 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./index.html"),
+    }),
+  ],
+};
