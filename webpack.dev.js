529cb896e36a0b93f81bd2ebf883f753d7d07c89
xing-6464 <17621404964@163.com>
2024-07-29 22:54:16 +0800

    webpack的插件学习

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -18,11 +19,28 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./index.html"),
+      title: "test", // 网页title名
+      filename: "app.html", // 文件名输出
+      template: path.resolve(__dirname, "./index.html"), // html模板路径
+      templateParameters: {
+        // html模板替换
+        titleName: "test2",
+      },
+      publicPath: "https://a.b.c/assets/", // 配置打包之后的引用路径
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[name].css",
     }),
   ],
+  optimization: {
+    minimizer: [
+      // css压缩插件
+      new CssMinimizerPlugin({
+        // test: /index\.css$/, // 匹配到的文件名
+        include: /\.css$/, // 匹配到的文件类型
+        exclude: /\.min\.css$/, // 排除的文件类型
+      }),
+    ],
+  },
 };
